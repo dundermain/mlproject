1e6fc9f23942c2f63e1587bd9cf44971f1ada1c2
dundermain <sachin68834@gmail.com>
2024-09-26 20:55:14 +0530

    added data ingestion pipeline

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:  #it is better to use @dataclass if you are only defining a variable, if you are planning to use any new function inside class better use dunder methods
+    train_data_path:str = os.path.join('artifacts', "train.csv")
+    test_data_path:str = os.path.join('artifacts', "test.csv")
+    raw_data_path:str = os.path.join('artifacts', "data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)            #this line takes the data from stud.csv and saves it in data.csv
+            logging.info("Train test split initiated")
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=69)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)   #this line takes the data from raw data i.e data.csv and saves it in train.csv
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)     #this line takes the data from raw data i.e data.csv and saves it in test.csv
+
+            logging.info("Ingestion of data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
