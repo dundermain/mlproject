725249578526ce78986442b004e7886dbeca78b1
dundermain <sachin68834@gmail.com>
2024-09-27 12:53:16 +0530

    added data transformation pipleine

--- src/exception.py
+++ src/exception.py
@@ -7,7 +7,7 @@ def error_message_details(error, error_detail:sys):
 
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
-    error_message = "Error occured in Python script name [{0}] line number [{1}] error message [{2}]".format(file_name, line_number, str(error))
+    error_message = f"Error occured in Python script name [{file_name}] line number [{line_number}] error message [{error}]"
 
     return error_message
 
